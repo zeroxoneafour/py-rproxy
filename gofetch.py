28d11ad20e194fd1ea08c344f80184b72460fe40
Vaughan Milliman <vaughanm1@protonmail.com>
2021-08-17 17:17:18 -0500

    pre-commit for removing gateway completely... ?

--- gofetch.py
+++ gofetch.py
@@ -38,21 +38,17 @@ def read_website(url, gatewaypath):
 
 # download a website without downloading website dependencies
 def download_website(url, gateway, gatewaypath):
-    if not (os.path.isdir(gatewaypath + url_to_path(url)) or os.path.isfile(gatewaypath + url_to_path(url))):
+    mimetype = mimetypes.guess_type(url)[0]
+    if (mimetype is None):
+        mimetype = "text/html"
+    if (mimetype.find("image") != -1) or (mimetype.find("video") != -1) or (mimetype.find("application") != -1):
         r = requests.get(url)
-        if not ".html" in r.url:
-            if (r.url[-1] == '/'):
-                mimetype = 'text/html'
-                os.makedirs(os.path.dirname(gatewaypath + url_to_path(r.url) + "index.html"), exist_ok=True)
-                filepath = gatewaypath + url_to_path(r.url) + 'index.html'
-            else:
-                mimetype = mimetypes.guess_type(url)
-                os.makedirs(os.path.dirname(gatewaypath + url_to_path(r.url)), exist_ok=True)
-                filepath = gatewaypath + url_to_path(r.url)
+        if not (os.path.isdir(gatewaypath + url_to_path(r.url)) or os.path.isfile(gatewaypath + url_to_path(r.url))):
+            os.makedirs(os.path.dirname(gatewaypath + url_to_path(r.url)), exist_ok=True)
+            filepath = gatewaypath + url_to_path(r.url)
             if not (os.path.isfile(filepath)):
                 f = open(filepath, 'x')
                 f.close()
-            print(filepath)
             f = open(filepath, "wb")
             content = r.content
             f.write(content)
@@ -65,9 +61,8 @@ def fix_page(page, url, gateway, gatewaypath):
     page = re.sub("\"\/", "\"https://" + urlparse(url).netloc + "/", page)
     # multithreading? idk
     with concurrent.futures.ThreadPoolExecutor() as executor:
-        response_process = []
         for url in re.findall("\"https?:\/\/[^\"]*", page):
-            response_process.append(executor.submit(download_website, re.sub("\"",'', url), gateway, gatewaypath))
+            executor.submit(download_website, re.sub('\"','',url), gateway, gatewaypath)
     page = re.sub("\"https?:\/", "\"/" + gateway, page)
     return page.encode("utf-8", "ignore")
 
@@ -88,7 +83,9 @@ def fetch_website(url, gateway, gatewaypath):
     print(filepath)
     f = open(filepath, "wb")
     content = r.content
-    content = fix_page(content, r.url, gateway, gatewaypath)
+    mimetype = mimetypes.guess_type(filepath)[0]
+    if (mimetype is None) or (mimetype.find("text") != -1):
+        content = fix_page(content, r.url, gateway, gatewaypath)
     f.write(content)
     f.close
     return
